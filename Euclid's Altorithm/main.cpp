933439dc72d465bb8262325630b197195d51ec61
AThreeDimensionalLemon <eisigliang@gmail.com>
2025-04-25 21:06:07 -0700

    Added comments as per requirements

--- Euclid's Altorithm/main.cpp
+++ Euclid's Altorithm/main.cpp
@@ -8,7 +8,7 @@ int static euclid(int num1, int num2) {
     if (num1 == 0)
         return num2;
 
-    return euclid(num2 % num1, num1);
+    return euclid(num2 % num1, num1); //Keep finding modulos until it's 0, then return the other number
 }
 
 int static hand(int num1, int num2) {
@@ -18,13 +18,13 @@ int static hand(int num1, int num2) {
     std::vector<int> num1Divisors;
     std::vector<int> num2Divisors;
 
-    for (int i = 1; i <= num1; i++) {
+    for (int i = 1; i <= num1; i++) { //Find all divisors for num1
 
         if (num1 % i == 0)
             num1Divisors.push_back(i);
     }
 
-    for (int i = 1; i <= num2; i++) {
+    for (int i = 1; i <= num2; i++) { //Do it again for num2
 
         if (num2 % i == 0)
             num2Divisors.push_back(i);
@@ -34,7 +34,7 @@ int static hand(int num1, int num2) {
 
         for (auto j = num2Divisors.rbegin(); j != num2Divisors.rend(); j++) {
 
-            if (*i == *j)
+            if (*i == *j) //Find the divisor that matches, starting from the greatest
                 return *j;
         }
     }
